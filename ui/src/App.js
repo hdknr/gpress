dcd6046fcd3f94ad5056d4f956d61f1fc666b000
user.name <gmail@hdknr.com>
2020-07-18 14:29:45 +0900

    send cookie

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,18 +5,55 @@ import gql from "graphql-tag";
 import ApolloClient from 'apollo-boost';
 import { Query } from "react-apollo";
 import Cookies from 'js-cookie'
+// import { createHttpLink, HttpLink } from "apollo-link-http";
+import { HttpLink } from "apollo-link-http";
+// import { InMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache} from 'apollo-cache-inmemory';
+
+
+
+const URI = "http://ubn1804:9000/api/graphql/"
+
+/*
+const link1 = createHttpLink({
+  uri: URI,
+  //credentials: 'same-origin'
+  credentials: 'include'
+});
+*/
 
 const client = new ApolloClient({
-  uri: "http://ubn1804:9000/api/graphql/",
+  uri: URI,
   credentials: 'include',
   request: (operation) => {
+    console.log(operation.getContext())
+    console.log(operation)
     const csrfToken = Cookies.get('csrftoken')
     operation.setContext({
       headers: { 
-        "X-CSRFTOKEN": csrfToken }
+        "X-CSRFToken": csrfToken ,
+        'X-Requested-With': 'XMLHttpRequest',
+      }
     })
   },
 })
+
+/*
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: '/graphql/xxxxxx/',
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+*/
+
+// const link = new HttpLink({ uri: URI });
+// const client = new ApolloClient({link: link})
+
+//////////
 
 const PostSet = () => (
   <Query
