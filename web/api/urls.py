3bef8c775b4c4bc9e3f4635a32175d0be71d6250
Ubuntu <ubuntu@ip-172-31-38-159.ap-northeast-1.compute.internal>
2020-07-19 13:48:04 +0900

    api: auth/login/

--- web/api/urls.py
+++ web/api/urls.py
@@ -1,8 +1,11 @@
 from graphene_django.views import GraphQLView
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
     path('graphql/', GraphQLView.as_view(graphiql=True)),
     path('rest/gpress/', include('gpress.api.urls')),
+    path('auth/', include('rest_framework.urls')),
+    path('', views.index),
 ]
