6e11d479ada0fb20092fa4fb4e482cf05fde1800
user.name <gmail@hdknr.com>
2020-07-19 16:36:54 +0900

    login_required for index view

--- web/api/views.py
+++ web/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
-
+@login_required
 def index(request):
     meta = dict((k, v) for (k, v) in request.META.items())
     ctx = {'name': 'index', 'meta': meta}
