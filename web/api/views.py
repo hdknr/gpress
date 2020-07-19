a11eb67fa0619416d08d6e9d2b7b10866afff350
Ubuntu <ubuntu@ip-172-31-38-159.ap-northeast-1.compute.internal>
2020-07-19 14:23:39 +0900

    debug index view

--- web/api/views.py
+++ web/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    meta = dict((k, v) for (k, v) in request.META.items())
+    ctx = {'name': 'index', 'meta': meta}
+    return render(request, 'api/index.html',context=ctx)
