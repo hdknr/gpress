f8d38e4616cb87f25323aee844bcbb9c0e853617
user.name <gmail@hdknr.com>
2020-07-20 16:51:43 +0900

    apibase.schema.BaseSerializerMutation

--- gpress/api/mutation.py
+++ gpress/api/mutation.py
@@ -3,30 +3,22 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphene_django.rest_framework.mutation import SerializerMutation
 from graphql_relay import from_global_id
+from apibase.schema import BaseSerializerMutation
 from . import serializers
 
 
-class Post(SerializerMutation):
+class Post(BaseSerializerMutation):
     class Meta:
         serializer_class = serializers.PostSerializer
         model_operations = ['create', 'update']
         lookup_field = 'id'
 
 
-class Postmeta(SerializerMutation):
+class Postmeta(BaseSerializerMutation):
     class Meta:
         serializer_class = serializers.PostmetaSerializer
         model_operations = ['create', 'update']
         lookup_field = 'id'
-
-
-    @classmethod
-    def get_serializer_kwargs(cls, root, info, **input):
-        client_mutation_id = input.get('client_mutation_id', None) 
-        if isinstance(client_mutation_id, str):
-            _, id = from_global_id(client_mutation_id)
-            input['id'] = id
-        return super().get_serializer_kwargs(root, info, **input)
 
 
 class Mutation(graphene.ObjectType):
