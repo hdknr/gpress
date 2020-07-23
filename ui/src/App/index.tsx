7bf7eb5d715fa761269a9f77994f357e96ce1703
user.name <gmail@hdknr.com>
2020-07-23 09:39:19 +0900

    Main component

--- ui/src/App/index.tsx
+++ ui/src/App/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './index.css';
 import './App.css';
 import { Header } from "../shared/Header";
+import { Main } from "../shared/Main";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <Header/>
-    </div>
+      <Router>
+        <Header />
+        <Main /> 
+      </Router>
+    </div >
   );
 }
 
---
+++ ui/src/shared/Main/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export const Main: React.FunctionComponent = () => {
+    return (
+        <div className="App-Main">
+            <h2>Main</h2>
+        </div>);
+};
